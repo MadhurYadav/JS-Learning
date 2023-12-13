42ad5bcd45a5af2e8dd1bac5ef660cd77c4e4eef
Madhur Yadav <94036633+MadhurYadav@users.noreply.github.com>
2023-12-13 09:00:23 +0000

    array_prop learn config

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -13,19 +13,25 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
 
+// If there is complex array like this
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
+//Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+// depth can be 1,2,,,,,,,,infinity
+
 console.log(real_another_array);
 
 
 
 console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
+console.log(Array.from("Hitesh")) 
 console.log(Array.from({name: "hitesh"})) // interesting
+// we've sprcified that arrays of keys or value
+// that's why it gives empty array! []
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); //return new arr
