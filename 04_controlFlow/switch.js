669875b9ecec867a448933597a88d7fab6794d1b
Madhur Yadav <94036633+MadhurYadav@users.noreply.github.com>
2024-01-12 15:50:04 +0000

    switch commit

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// for copy (shift + alt + downarrow)
+
+// break is imp! Because if any case print then all other case will print after except default
