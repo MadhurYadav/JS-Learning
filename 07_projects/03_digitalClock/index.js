5d482600ae1b3f5fd068b425a6eab013bb2559de
Madhur Yadav <94036633+MadhurYadav@users.noreply.github.com>
2024-01-16 11:58:53 +0000

    digitalClock project

---
+++ 07_projects/03_digitalClock/index.js
@@ -0,0 +1,8 @@
+// we can use document.querySelector('#clock')
+const clock = document.getElementById('clock')
+
+setInterval(function(){
+    let date = new Date()
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+},1000);
