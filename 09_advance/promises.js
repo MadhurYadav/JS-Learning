ed2534a3e2780c4e30d0ee74f4edbefb7f9d01c0
Madhur Yadav <94036633+MadhurYadav@users.noreply.github.com>
2024-01-26 12:08:49 +0000

    promise

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -40,23 +40,75 @@ promiseThree.then(function(user){
     // console.log(user);
 })
 
-const promiseFour = new Promise(function(resolve, reject){
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if(!error){
+//             resolve({username:"madhur", password:"123"})
+//         }else{
+//             reject('ERROR SOMETHING WENT WRONG')
+//         }
+//     }, 1000)
+// })
+
+// // chaining
+// promiseFour.then((user)=>{
+//     // console.log(user);
+//     return user.username
+// }).then((username)=>{
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+//
+
+const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
         let error = true
-        if(!error){
-            resolve({username:"madhur", password:"123"})
-        }else{
-            reject('ERROR SOMETHING WENT WRONG')
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
         }
     }, 1000)
-})
+});
 
-// chaining
-promiseFour.then((user)=>{
-    // console.log(user);
-    return user.username
-}).then((username)=>{
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// next
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("Error", error);
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) =>{
+    return response.json()
+})
+.then((data) =>{
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
